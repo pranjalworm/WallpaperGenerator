faf53e3935ee6645e921cbe1e7aebed48da5b7c0
pranjal dubey <pjldby@gmail.com>
2015-12-25 00:17:31 +0530

    added code for generating triangles

--- wallpapers.py
+++ wallpapers.py
@@ -1,8 +1,8 @@
 from PIL import Image
-import random, sys
+import random, sys, os
 
 '''
-#creating color tiles: NOT DONE
+#making the program more modular
 def createColorTiles2():
     colors = {
                 'red' : (156, 58, 58, 255),
@@ -19,7 +19,85 @@ def createColorTiles2():
         colorImageHandle.save('colors/' + color + '.jpg')
 '''
 
-#creating color tiles
+def createPixelColorTiles():
+    '''
+    creating 1x1 pixel size tiles
+    '''
+
+    #creating ORANGE color 1 pixel tile
+    imageHandleOrange = Image.new('RGBA', (1, 1), (205, 140, 64, 255))
+    imageHandleOrange.save('colors/orange-1p.jpg')
+
+    #creating PURPLE color 1 pixel tile
+    imageHandlePurple = Image.new('RGBA', (1, 1), (124, 72, 149, 255))
+    imageHandlePurple.save('colors/purple-1p.jpg')
+
+    #creating RED color 1 pixel tile
+    imageHandleRed = Image.new('RGBA', (1, 1), (156, 58, 58, 255))
+    imageHandleRed.save('colors/red-1p.jpg')
+
+    #creating BLUE color 1 pixel tile
+    imageHandleBlue = Image.new('RGBA', (1, 1), (49, 69, 131, 255))
+    imageHandleBlue.save('colors/blue-1p.jpg')
+
+    #creating GREEN color 1 pixel tile
+    imageHandleGreen = Image.new('RGBA', (1, 1), (58, 124, 65, 255))
+    imageHandleGreen.save('colors/green-1p.jpg')
+
+    #creating GREY color 1 pixel tile
+    imageHandleGrey = Image.new('RGBA', (1, 1), (86, 88, 88, 255))
+    imageHandleGrey.save('colors/grey-1p.jpg')
+
+
+def createTriangleTiles():
+    '''
+    create image tile with triangle in foreground on a square background
+    '''
+    imageBackgroundHandle1 = Image.open('colors/grey.jpg')
+    imageBackgroundHandle2 = Image.open('colors/grey.jpg')
+    xLimit, yLimit = imageBackgroundHandle1.size
+
+    imageForegroundHandle = Image.open('colors/red-1p.jpg')
+
+    for y in range(yLimit):
+        for x in range(y):
+            imageBackgroundHandle1.paste(imageForegroundHandle, (x, y))
+
+    for x in range(xLimit):
+        for y in range(x):
+            imageBackgroundHandle2.paste(imageForegroundHandle, (x, y))
+
+    os.chdir("c:/pythoncode/programs/wallpapergenerator/colors")
+    imageBackgroundName = 'grey-red-triangle-tile1.jpg'
+    imageBackgroundHandle1.save(imageBackgroundName)
+    imageBackgroundName = 'grey-red-triangle-tile2.jpg'
+    imageBackgroundHandle2.save(imageBackgroundName)
+
+def createTriangleWallpaper():
+    '''
+    create wallpapers with triangle tiles
+    '''
+
+    os.chdir("c:/pythoncode/programs/wallpapergenerator")
+
+    imageBackgroundHandle = Image.new('RGBA', (1000, 1900))
+    xLimit, yLimit = imageBackgroundHandle.size
+
+    imageTriangleHandle1 = Image.open('colors/grey-red-triangle-tile1.jpg')
+    imageTriangleHandle2 = Image.open('colors/grey-red-triangle-tile2.jpg')
+
+    colorTiles = [imageTriangleHandle1, imageTriangleHandle2]
+
+    for y in range(0, yLimit, 100):
+        for x in range(0, xLimit, 100):
+            colorTile = random.randint(0, len(colorTiles) - 1)
+            imageBackgroundHandle.paste(colorTiles[colorTile], (x, y))
+
+    os.chdir("c:/pythoncode/programs/wallpapergenerator/output/mobile")
+    imageBackgroundName = 'grey-red-triangle.jpg'
+    imageBackgroundHandle.save(imageBackgroundName)
+
+
 def createColorTiles():
     #creating PURPLE color tile
     imageHandlePurple = Image.new('RGBA', (100, 100), (124, 72, 149, 255))
@@ -30,19 +108,23 @@ def createColorTiles():
     imageHandleRed.save('colors/red.jpg')
 
     #creating BLUE color tile
-    imageHandleRed = Image.new('RGBA', (100, 100), (49, 69, 131, 255))
-    imageHandleRed.save('colors/blue.jpg')
+    imageHandleBlue = Image.new('RGBA', (100, 100), (49, 69, 131, 255))
+    imageHandleBlue.save('colors/blue.jpg')
 
     #creating GREEN color tile
-    imageHandleRed = Image.new('RGBA', (100, 100), (58, 124, 65, 255))
-    imageHandleRed.save('colors/green.jpg')
-
+    imageHandleGreen = Image.new('RGBA', (100, 100), (58, 124, 65, 255))
+    imageHandleGreen.save('colors/green.jpg')
     #creating ORANGE color tile
-    imageHandleRed = Image.new('RGBA', (100, 100), (205, 140, 64, 255))
-    imageHandleRed.save('colors/orange.jpg')
 
-#creating background image
-def createBackground(orientation, imageFormat, style):
+    imageHandleOrange = Image.new('RGBA', (100, 100), (205, 140, 64, 255))
+    imageHandleOrange.save('colors/orange.jpg')
+
+    #creating GREY color tile
+    imageHandleGrey = Image.new('RGBA', (100, 100), (59, 60, 60, 255))
+    imageHandleGrey.save('colors/grey.jpg')
+
+#creating wallpaper image
+def createSquareWallpaper(orientation, imageFormat, style):
     createColorTiles()
 
     imageHandleRed = Image.open('colors/red.jpg')
@@ -55,16 +137,12 @@ def createBackground(orientation, imageFormat, style):
 
     #choosing image orientation
     if orientation == 'mobile':
-        backgroundHandle = Image.new('RGBA', (400, 600))
+        wallpaperHandle = Image.new('RGBA', (400, 600))
         xLimit = 401
         yLimit = 601
-    elif orientation == 'desktop':
-        backgroundHandle = Image.new('RGBA', (1200, 800))
-        xLimit = 1201
-        yLimit = 801
     else:
-        backgroundHandle = Image.new('RGBA', (800, 800))
-        xLimit = 801
+        wallpaperHandle = Image.new('RGBA', (1200, 800))
+        xLimit = 1201
         yLimit = 801
 
     colorTile = 0
@@ -74,19 +152,19 @@ def createBackground(orientation, imageFormat, style):
         for y in range(0, yLimit, 100):
             colorTile = random.randint(0, len(colorTiles) - 1)
             for x in range(0, xLimit, 100):
-                backgroundHandle.paste(colorTiles[colorTile], (x, y))
+                wallpaperHandle.paste(colorTiles[colorTile], (x, y))
 
     elif style == 'columns':
         for x in range(0, xLimit, 100):
             colorTile = random.randint(0, len(colorTiles) - 1)
             for y in range(0, yLimit, 100):
-                backgroundHandle.paste(colorTiles[colorTile], (x, y))
+                wallpaperHandle.paste(colorTiles[colorTile], (x, y))
 
     elif style == 'random':
         for y in range(0, yLimit, 100):
             for x in range(0, xLimit, 100):
                 colorTile = random.randint(0, len(colorTiles) - 1)
-                backgroundHandle.paste(colorTiles[colorTile], (x, y))
+                wallpaperHandle.paste(colorTiles[colorTile], (x, y))
 
     else:
         count = 0
@@ -94,7 +172,7 @@ def createBackground(orientation, imageFormat, style):
             for x in range(0, xLimit, 100):
                 colorTile = count % len(colorTiles)
                 count += 1
-                backgroundHandle.paste(colorTiles[colorTile], (x, y))
+                wallpaperHandle.paste(colorTiles[colorTile], (x, y))
 
     #choosing image format
     if imageFormat == 'png':
@@ -103,19 +181,20 @@ def createBackground(orientation, imageFormat, style):
         imageFormat = '.jpg'
 
     #creating image filename
-    backgroundName = orientation + '-' + style + imageFormat
-    backgroundHandle.save(backgroundName)
+    wallpaperName = orientation + '-' + style + imageFormat
+    os.chdir("c:/pythoncode/programs/wallpapergenerator/output/" + orientation)
+    wallpaperHandle.save(wallpaperName)
 
-    print('\nAll done...!\t' + backgroundName + ' created!')
+    print('\nAll done...!\t' + wallpaperName + ' created!')
 
 
-#taking background specifications from user
-def getBackgroundSpecs():
-    orientation = input('\nEnter the orientation of background [mobile/desktop/square]: ')
+#taking wallpaper specifications from user
+def getWallpaperSpecs():
+    orientation = input('\nEnter the orientation of wallpaper [mobile/desktop]: ')
 
     imageFormat = input('\nEnter the image format [jpg/png]: ')
 
-    style = input('\nEnter background style [boxes/rows/columns/random]: ')
+    style = input('\nEnter wallpaper style [boxes/rows/columns/random]: ')
 
     if orientation != 'mobile' and orientation != 'desktop' and orientation != 'square':
         print('Invalid orientation entered!')
@@ -126,13 +205,16 @@ def getBackgroundSpecs():
         imageFormat = 'jpg'
 
     if style != 'boxes' and style !='rows' and style != 'columns' and style != 'random':
-        print('Invalid background style entered!')
+        print('Invalid style entered!')
         style = 'boxes'
 
 
-    createBackground(orientation, imageFormat, style)
+    createSquareWallpaper(orientation, imageFormat, style)
 
 #RUN PROGRAM
-getBackgroundSpecs()
+#getWallpaperSpecs()
 
-#createColorTiles2()
+createColorTiles()
+createPixelColorTiles()
+createTriangleTiles()
+createTriangleWallpaper()
